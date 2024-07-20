8375bc5a0e2860ed34f0b81c6cdc09a27def8728
Coder20233 <evanvictorius30@gmail.com>
2024-07-20 22:55:47 +0700

    Inntial commit
    Final Project

--- FinalProject/WeatherApplication/index2.js
+++
@@ -1,8 +0,0 @@
-const trailer = document.getElementById("trailer")
-
-window.onmousemove = e => {
-    const x = e.clientX,
-          y = e.clientY;
-
-          trailer.style.transform =`translate(${x}px, ${y}px)`;
-}
